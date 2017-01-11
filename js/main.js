e51731023b50fcd55f2728cdac366f1123a8d41b
beijixing <long_555@126.com>
2017-01-12 00:18:40 +0800

    完成6.10

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
-
+    alert("hello1");
+$('.navbar [href^=#]').click(function (e) {
+    //alert("hello2");
+    e.preventDefault();
+   // alert("hello3");
+    var div = $(this).attr('href');
+    $("html, body").animate({
+        scrollTop:$(div).position().top
+    }, "slow");
+    //alert("hello4");
+});
 
 
 });
